2deb724074829cd4addafe1ed266b654ab62ef71
Eunsong Koh <eunsongkohh@gmail.com>
2025-02-08 23:01:43 -0800

    Initial Commit

---
+++ WeatherDemo/WeatherDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherDemoApp.swift
+//  WeatherDemo
+//
+//  Created by Eunsong Koh on 2025-02-08.
+//
+
+import SwiftUI
+
+@main
+struct WeatherDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
